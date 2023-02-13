52c7301af78114e56932d47cba992268e76b1b0f
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2023-02-13 18:16:15 +1100

    create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -130,7 +130,10 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
-        render json: @user, status: :created
+        if @user
+            @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+            render json: @user, status: :created
+        end
     end
 
 
