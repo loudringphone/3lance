014d80578ebaf4aae4db7c1665960292663de206
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2023-02-15 09:48:57 +1100

    amend the offer model so that only one open request for a request from an user each time

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,16 @@
 class Offer < ApplicationRecord
     belongs_to :user, :optional => true
     belongs_to :request, :optional => true
-    # validates :request_id, presence: true, uniqueness: { scope: :user_id }
-    # validates :user_id, presence: true, uniqueness: { scope: :request_id }
+    validates :user_id, presence: true
+    validate :only_one_open_offer_per_user
+
+    def open?
+        status == 'Open'
+    end
+
+    def only_one_open_offer_per_user
+        if self.open? && Offer.exists?(user_id: self.user_id, request_id: self.request_id, status: 'Open'){ |offer| offer.id != self.id }
+          errors.add(:base, 'You cannot have more than one open offer at a time')
+        end
+      end
 end
