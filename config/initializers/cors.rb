8a844df6429f6331f7320c8046aca2da28814577
loudringphone <loudringphone@gmail.com>
2023-02-20 10:51:55 +1100

    amending cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ['https://threelancers.herokuapp.com', 'https://3lancers.netlify.app/', 'http://localhost:3001']
+      origins 'https://threelancers.herokuapp.com', 'https://3lancers.netlify.app', 'http://localhost:3001'
       resource '*',
         :headers => :any,
         :methods => %i(get post put patch delete options head)
