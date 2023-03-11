f098bd857732704b9d7773cb80d14e909d00d402
loudringphone <loudringphone@gmail.com>
2023-03-12 01:00:14 +1100

    deleted some commented lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user
-            # @token = JWT.encode({user_id: @user.id, username: @user.username}, Rails.application.secrets.secret_key_base[0])
             @token = JWT.encode({user_id: @user.id, username: @user.username}, ENV["RAILS_SECRET_KEY_BASE"][0])
             render json: @user, status: :created
         end
@@ -25,7 +24,6 @@ class UsersController < ApplicationController
         @user = User.find_by(email: params[:user][:email])
 
         if @user && @user.authenticate(params[:user][:password])
-        #   @token = JWT.encode({user_id: @user.id, username: @user.username}, Rails.application.secrets.secret_key_base[0])
         @token = JWT.encode({user_id: @user.id, username: @user.username}, ENV["RAILS_SECRET_KEY_BASE"][0])
           render json: {user: @user, token: @token}
         else
